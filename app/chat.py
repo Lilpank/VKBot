91e3ba0e23d2503b10c54938ccf35a7afec49c86
Lilpank <70665037+Lilpank@users.noreply.github.com>
2022-08-24 15:37:20 +0300

    change chat

--- app/chat.py
+++ app/chat.py
@@ -1,4 +1,4 @@
-from app import vk_bot
+import vk_bot
 from database import Database
 import logging
 import time
@@ -10,36 +10,14 @@ logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO,
                     datefmt="%H:%M:%S")
 
 
-def _connection_to_db():
+def _schledule_choice_slave():
     while True:
-        try:
-            db = Database()
-            break
-        except Exception as err:
-            logging.info("Doesn't connection to db")
-            time.sleep(5)
-    return db
-
-
-db = _connection_to_db()
-
-
-def choice_slave_from_db(chat_id):
-    users_id = db.select_data(f"select distinct user_id from participants where id_chat='{chat_id}'")
-    user_id = random.choice(users_id)[0]
-    count = db.select_data(
-        f"select count from participants where user_id='{user_id}' and id_chat='{chat_id}' limit 1")[0][0] + 1
-
-    db.update_data(
-        f"update participants set count='{count}' where user_id='{user_id}' and id_chat='{chat_id}'")
-
-    logging.debug(f"update count in table participants with user_id={user_id}, count={count}")
-
-    vk_bot.sender(f'@id{user_id}, отличного дня! ', chat_id)
+        schedule.run_pending()
 
 
 class Chat:
-    def __init__(self, chat_id):
+    def __init__(self, chat_id, db):
+        self.db = db
         self.chat_id = chat_id
 
         db.insert_value_into_table(f"INSERT INTO chats(id_chat) values('{chat_id}')"
@@ -48,27 +26,35 @@ class Chat:
 
         self.users = db.select_data(f"Select distinct user_id from participants where id_chat = {chat_id}")
         if len(self.users) != 0:
-            self.users = self.users[0]
+            self.users = list(self.users[0])
+        schedule.every().day.at("15:03").do(self.choice_slave_from_db, self.chat_id)
 
-        schedule.every().day.at("09:30").do(choice_slave_from_db, self.chat_id)
-
-        thread = threading.Thread(target=self._schledule_choice_slave)
+        thread = threading.Thread(target=_schledule_choice_slave)
         thread.start()
-
-    def _schledule_choice_slave(self):
-        while True:
-            schedule.run_pending()
 
     def save_userid_in_db(self, user_id):
         if user_id not in self.users:
             self.users.append(user_id)
-            db.insert_value_into_table(
+            self.db.insert_value_into_table(
                 f"INSERT INTO participants(id_chat, user_id) values('{self.chat_id}', '{user_id}')")
             logging.info(f'user_id: {user_id} insert into database')
         else:
             logging.info(f'user_id: {user_id} exists in db')
 
         logging.info(f'users list: {self.users}')
+
+    def choice_slave_from_db(self, chat_id):
+        users_id = self.db.select_data(f"select distinct user_id from participants where id_chat='{chat_id}'")
+        user_id = random.choice(users_id)[0]
+        count = self.db.select_data(
+            f"select count from participants where user_id='{user_id}' and id_chat='{chat_id}' limit 1")[0][0] + 1
+
+        self.db.update_data(
+            f"update participants set count='{count}' where user_id='{user_id}' and id_chat='{chat_id}'")
+
+        logging.debug(f"update count in table participants with user_id={user_id}, count={count}")
+
+        vk_bot.sender(f'@id{user_id}, отличного дня! ', chat_id)
 
     def get_chat_id(self):
         return self.chat_id
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import logging
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from chat import Chat
+import chat
+from database import Database
 from config import VK_SESSION, ID_BOT
+import time
 
 logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO,
                     datefmt="%H:%M:%S")
@@ -10,18 +12,40 @@ longpoll = VkBotLongPoll(VK_SESSION, ID_BOT)
 rooms_dict = {}
 
 
+def _connection_to_db():
+    while True:
+        try:
+            db = Database()
+            break
+        except Exception as err:
+            logging.info("Doesn't connection to db")
+            time.sleep(5)
+    return db
+
+
+db = _connection_to_db()
+
+
 def main():
     try:
+        chats = db.select_data("SELECT id_chat from chats")
+        if len(chats) != 0:
+            for room_id in chats:
+                rooms_dict[int(room_id[0])] = chat.Chat(room_id[0], db)
+
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
                 chat_id = event.chat_id
 
                 if chat_id not in rooms_dict:
                     try:
-                        rooms_dict[chat_id] = Chat(event.chat_id)
+                        rooms_dict[chat_id] = chat.Chat(event.chat_id, db)
                         logging.info(f"Create the room with chat_id: {chat_id}")
                     except Exception as err:
                         logging.error(err)
+                else:
+                    logging.info("This room is exist.")
+
                 if event.from_chat:
                     msg = event.object.message['text'].lower()
                     logging.info(f'User send info - {msg}')
